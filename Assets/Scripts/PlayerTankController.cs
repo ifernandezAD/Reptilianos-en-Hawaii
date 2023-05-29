7a27d5121f8d718d196e62deb2a109513559d9cf
IFernandezAD <ifernandezad@gmail.com>
2023-05-29 21:08:49 +0200

    Starter assets

---
+++ Assets/Scripts/PlayerTankController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerTankController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
+
+    private CharacterController myCharacterController;
+
+    private void Awake()
+    {
+        myCharacterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            myCharacterController.Move(transform.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            myCharacterController.Move(-transform.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
+
+
+    }
+}

07c99e5e8f1a0a465231b1f6a4bdd369d6e37d21
IFernandezAD <ifernandezad@gmail.com>
2023-06-02 19:18:12 +0200

    Handle Animator script created

---
+++ Assets/AnimatorManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    float snappedHorizontal;
+    float snappedVertical;
+
+    public void HandleAnimatorValues(float horizontalMovement, float verticalMovement)
+
+    {
+        if (horizontalMovement > 0)
+        {
+            snappedHorizontal = 1;
+        }
+        else if (horizontalMovement < 0)
+        {
+            snappedHorizontal = -1;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+
+        if (verticalMovement > 0)
+        {
+            snappedVertical = 1;
+        }
+        else if (verticalMovement < 0)
+        {
+            snappedVertical = -1;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+
+        animator.SetFloat("Horizontal", snappedHorizontal, 0.1f, Time.deltaTime);
+        animator.SetFloat("Vertical", snappedVertical, 0.1f, Time.deltaTime);
+    }
+}
